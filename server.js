6a91af3387c62a6a1e324992302bcb73e419492a
Ruba <rsharif24@hotmail.com>
2017-08-08 21:54:04 -0500

    setup server.js file

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+var PORT = 8000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.listen(PORT);
