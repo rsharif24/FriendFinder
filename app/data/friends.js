ca809613befab96c71b7aa893a2d54b00a15682a
Ruba <rsharif24@hotmail.com>
2017-08-12 12:42:44 -0500

    setup apiRoutes.js

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+var friends = [
+  {
+    name: "Bob",
+    photo: "www.google.com",
+    scores: ["5","1","4","4","5","1","2","5","4","1"]
+  }
+];
+
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 var app = express();
 
 var PORT = 8000;
@@ -9,4 +10,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.listen(PORT);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
