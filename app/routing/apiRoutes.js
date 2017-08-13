e1459b0ad19cd978f038715aa09ebc9e1f023e3b
Ruba <rsharif24@hotmail.com>
2017-08-12 23:08:41 -0500

    created formula for match calculations

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,52 @@
 var friendsData = require("../data/friends");
 
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-  app.get("/api/friends", function(req, res) {
+  app.get("/api/friends", function (req, res) {
     res.json(friendsData);
   });
 
-  app.post("/api/friends", function(req, res) {
-      friendsData.push(req.body);
+  app.post("/api/friends", function (req, res) {
+
+
+    var newFriend = req.body.scores;
+    var friendMatch = {
+      name: "",
+      photo: "",
+      matchDiff: 100
+    };
+
+
+
+    for (var i = 0; i < friendsData.length; i++) {
+
+      totalDifference = 0;
+
+      for (var x = 0; x < newFriend.length; x++) {
+
+        totalDifference += Math.abs(+(newFriend[x]) - +(friendsData[i].scores[x]));
+
+      };
+
+      if (totalDifference < friendMatch.matchDiff) {
+
+        friendMatch.name = friendsData[i].name;
+        friendMatch.photo = friendsData[i].photo;
+        friendMatch.matchDiff = totalDifference;
+      };
+    };
+
+    friendsData.push(req.body);
+    res.json(friendMatch);
+
+  });
+
+  app.post("/api/clear", function() {
+
+    friendsData = [];
+  
+
   });
 
 };
