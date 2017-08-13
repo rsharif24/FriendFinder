10c7c31df9aecd36854476a7c62cdbe846a4ee33
Ruba <rsharif24@hotmail.com>
2017-08-13 16:50:30 -0500

    added validation and styled html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,18 @@
 var friends = [
   {
-    name: "Bob",
-    photo: "www.google.com",
+    name: "Godfrey",
+    photo: "https://pbs.twimg.com/profile_images/577440317844094977/i3eTxXhf.jpeg",
     scores: ["5","1","4","4","5","1","2","5","4","1"]
+  },
+  {
+    name:"Gertrude",
+    photo:"https://uploads1.wikiart.org/images/fernando-botero/pope-leo-x-after-raphael.jpg!PinterestSmall.jpg",
+    scores:["3","5","5","1","1","2","4","3","2","1"]
+  },
+  {
+    name:"Eustace",
+    photo:"https://s-media-cache-ak0.pinimg.com/736x/70/0f/5a/700f5a4deeb8358a3576f791134d3442--famous-poets-the-renaissance.jpg",
+    scores:["4","2","1","3","5","3","1","1","2","5"]
   }
 ];
 
